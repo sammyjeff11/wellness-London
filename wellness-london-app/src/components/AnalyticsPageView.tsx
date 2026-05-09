900ec8706167cd76167ae56214617a4b8941d639
sammyjeff11 <samjeffreys1@gmail.com>
2026-05-09 19:09:31 +0100

    Add page view analytics component

---
+++ wellness-london-app/src/components/AnalyticsPageView.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import { useEffect } from "react";
+import { trackEvent, type AnalyticsProperties } from "@/lib/analytics";
+
+type AnalyticsPageViewProps = {
+  eventName: "service_page_view" | "facility_page_view";
+  properties?: AnalyticsProperties;
+};
+
+export default function AnalyticsPageView({ eventName, properties }: AnalyticsPageViewProps) {
+  useEffect(() => {
+    trackEvent(eventName, properties);
+  }, [eventName, properties]);
+
+  return null;
+}
