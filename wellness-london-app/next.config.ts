bf512e2208b1b0eb89479b9dfa865813861564a0
sammyjeff11 <samjeffreys1@gmail.com>
2026-05-09 11:25:23 +0100

    Configure images on main

--- wellness-london-app/next.config.ts
+++ wellness-london-app/next.config.ts
@@ -1,7 +1,21 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  turbopack: {
+    root: process.cwd(),
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**.airtableusercontent.com",
+      },
+      {
+        protocol: "https",
+        hostname: "dl.airtable.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
