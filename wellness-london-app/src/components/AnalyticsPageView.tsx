3cfa00c7cb5e46fd6b6f2f8ef6d4ddef1b3146cc
sammyjeff11 <samjeffreys1@gmail.com>
2026-05-10 20:00:53 +0100

    Allow location page analytics event

--- wellness-london-app/src/components/AnalyticsPageView.tsx
+++ wellness-london-app/src/components/AnalyticsPageView.tsx
@@ -4,7 +4,7 @@ import { useEffect } from "react";
 import { trackEvent, type AnalyticsProperties } from "@/lib/analytics";
 
 type AnalyticsPageViewProps = {
-  eventName: "service_page_view" | "facility_page_view";
+  eventName: "service_page_view" | "facility_page_view" | "location_page_view";
   properties?: AnalyticsProperties;
 };
 
